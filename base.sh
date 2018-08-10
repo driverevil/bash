d813fc815113f3c44a2b3082156a246865ac517f
ruthven <driverevil@gmail.com>
2018-08-10 16:39:05 -0500

    Unue sendu

---
+++ base.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Simpla basa script
+
+echo Saluton al cxiuj!
+
---
+++ simple_copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A simple copy script
+
+cp $1 $2
+
+# Let's verify the copy worked
+
+echo Detaloj por: $2
+ls -lh $2
+
